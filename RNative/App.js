2e1af4f6964cca38f3ed72a070c38e158bba439a
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-06-14 16:40:11 -0300

    Add textInput and button of view

--- RNative/App.js
+++ RNative/App.js
@@ -1,19 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
+    <View style={styles.appContainer}>
+     <View style={styles.inputContainer}>
+       <TextInput style={styles.textInput} placeholder="Your course goal!"></TextInput>
+       <Button title="Add Goal"></Button>
+     </View>
+     <View>
+        <Text>List of goals...</Text>
+     </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ appContainer: {
+  padding: 50,
+ },
+ inputContainer: {
+   flexDirection: 'row',
+   justifyContent: 'space-between'
+ },
+ textInput: {
+  borderWidth: 1,
+  borderColor: '#cccccc',
+  width: '80%',
+  marginRight: 8,
+  padding: 8,
+ }
 });
